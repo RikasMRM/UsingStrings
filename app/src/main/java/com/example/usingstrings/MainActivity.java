811a458fb855c13a1df0382b20ac430d62bfb1a6
RikasMRM <rikasrkf@gmail.com>
2020-08-08 13:35:13 +0530

    test comment

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+//This is my tute 1
 public class MainActivity extends AppCompatActivity {
 
     TextView txtView2;
